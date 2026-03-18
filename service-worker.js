e5ba85efbfde3c492955d1b9fd2ad90f9aa77625
a825249584 <825249584@qq.com>
2026-03-18 20:46:35 +0800

    Add files via upload
    001

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'speech-pwa-v1';
+const urlsToCache = [
+  './001.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  // 可根据实际情况添加更多静态资源
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
